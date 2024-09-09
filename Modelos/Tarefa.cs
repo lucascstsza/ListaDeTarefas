f931a37301983ac452d02e65258575b7bd0a149c
Lucas Costa Souza <lucas_costasouza@yahoo.com.br>
2024-09-09 20:09:23 -0300

    Adicionar arquivos de projeto.

---
+++ Modelos/Tarefa.cs
@@ -0,0 +1,9 @@
+﻿namespace ListaDeTarefas.Modelos;
+
+public class Tarefa
+{
+    public int Id { get; set; }
+    public string? Titulo { get; set; }
+    public DateOnly Prazo { get; set; }
+    public bool Concluida { get; set; }
+}
